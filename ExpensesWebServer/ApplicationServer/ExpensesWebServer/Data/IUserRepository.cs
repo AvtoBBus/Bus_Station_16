fdbd3068e5e5938bd8bc61b04a201230a1d592df
mestromezer <global0908@mail.ru>
2023-11-12 14:58:22 +0300

    Separated auth controller to auth and reg. Added salt modofication for psswd.

---
+++ ExpensesWebServer/ApplicationServer/ExpensesWebServer/Data/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using ExpensesWebServer.Models.Entities;
+
+namespace ExpensesWebServer.Data
+{
+    public interface IUserRepository
+    {
+        public Task<int> CreateAsync(User user);
+        public Task<User> GetByLoginAsync(string user);
+        public Task<User> GetByIdAsync(int id);
+    }
+}
---
+++ ExpensesWebServer/ApplicationServer/ExpensesWebServer/Models/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExpensesWebServer.Models.Entities
+{
+    /*
+     * Класс User является основной сущностью для внутренней бизнес-логики
+     */
+    public class User
+    {
+        // Поле Id является UID для User
+        public int Id { get; set; }
+        // Логин User'a является уникальным
+        public string Login { get; set; }
+        // Пароль пользователя. Требования: https://support.kaspersky.com/KPC/1.0/ru-RU/183862.html
+        [JsonIgnore]
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        // Список трат содердится и обновляется с каждым месяцем.
+        // 
+        public List<Expense> Expenses { get; set; }
+    }
+}
